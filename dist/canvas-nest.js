260d0d9355b540fbccd4fcfd82079223acd1c161
hustcc <i@atool.org>
2017-01-03 10:09:29 +0800

    添加 license

--- dist/canvas-nest.js
+++ dist/canvas-nest.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2016 hustcc
+ * License: MIT
+ * Version: v1.0.1
+ * GitHub: https://github.com/hustcc/canvas-nest.js
+**/
 ! function() {
   //封装方法，压缩之后减少文件大小
   function get_attribute(node, attr, default_value) {
